ae84f9af15b92512fa802ef4d17b94ff8fa5d8e4
NeoFunnik <104463225+NeoFunnik@users.noreply.github.com>
2024-01-19 18:22:31 +0300

    Add files via upload

---
+++ scripy.js
@@ -0,0 +1,8 @@
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    alert('Сообщение отправлено!').
+    this.reset();
+  });
